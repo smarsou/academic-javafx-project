2e6c98e85d81faac5b97320bd62a4ec4ccc642cd
Serge Tehe <yohou.tehe@telecomnancy.eu>
2023-01-05 14:00:08 +0100

    Bug

--- src/main/java/eu/groupnine/codingweak/VueAccueilController.java
+++ src/main/java/eu/groupnine/codingweak/VueAccueilController.java
@@ -135,7 +135,7 @@ public class VueAccueilController implements Observer, Initializable{
         model.callObservers();
     }
 
-    public void refresh() throws InterruptedException{
+    public void refresh() {
         System.out.println("Refresh accueil");
         initialize(null, null);
     }
